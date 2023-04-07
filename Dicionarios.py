fc7b745fa333663271ea0a7d57389828faaabed5
Rhessaw0 <chryriathemaster@gmail.com>
2023-04-07 11:54:50 -0300

    Removing folder layer

---
+++ Dicionarios.py
@@ -0,0 +1,38 @@
+import json
+import pathlib
+import os
+
+Dic = {
+    'Admin': '5254302579',
+    'Saymon': 'senha',
+}
+
+def ler(file):
+    try:
+        nome = file
+        with open(nome, 'r') as input:
+            resultado = json.load(input)
+        print('pog')
+    except:
+        nome = file + '.json'
+        with open(nome, 'r') as input:
+            resultado = json.load(input)
+        print('sadge')
+    finally:
+        return resultado
+
+def escrever(file, dic):
+    with open((file), 'w') as output:
+        output.write(json.dumps(dic))
+
+def paraDic(file):
+    print()
+
+filepath = os.path.realpath(__file__)
+dirPath = os.path.dirname(filepath)
+
+print(Dic)
+
+Dic['Mano'] = 'senha'
+
+print(Dic)
